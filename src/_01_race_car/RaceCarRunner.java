5590c782e88e6ebe814b0da426361d4fdb45d781
student <student@ip-172-31-57-215.us-west-2.compute.internal>
2021-04-21 16:49:10 -0700

    kat

--- src/_01_race_car/RaceCarRunner.java
+++ src/_01_race_car/RaceCarRunner.java
@@ -1,4 +1,7 @@
 package _01_race_car;
+
+import javax.swing.JOptionPane;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2019
  *    Level 1
@@ -11,14 +14,30 @@ public class RaceCarRunner {
 		/* Do the following things without changing the RaceCar class */
 		
 		// 1. Create a RaceCar and place it in 5th position
-	
+	String name=JOptionPane.showInputDialog("What's your racing name?");
+	RaceCar car = new RaceCar("kart", 5);
+
 		// 2. Print the RaceCar's position in the race
-
+JOptionPane.showMessageDialog(null,"Commenter: Hey "+name+", you're in fifth place!");
 		// 3. Crash the RaceCar
-		
+JOptionPane.showMessageDialog(null, "a little while later");
+JOptionPane.showMessageDialog(null,"bonk");
+JOptionPane.showMessageDialog(null,"Commenter: Uh oh. Looks like "+name+" crashed their car!");
+car.crash();
 		// 4. If the car is damaged. Bring it in for a pit stop.
-
+car.isDamaged();
+car.pit();
+JOptionPane.showMessageDialog(null, "Commenter: Hey look, "+name+"'s made it! Three racers passed you while you were being pitted. ");
 		// 5. Help the car move into first place.
-
+car.overtake();
+JOptionPane.showMessageDialog(null, "Commenter: Looks like "+name+" made a close call with Jerry, but zooms ahead, unharmed. ");
+	car.overtake();
+	JOptionPane.showMessageDialog(null, "Commenter: Look,"+name+"'s really zoomin this time!");
+	car.overtake();
+	car.overtake();
+	car.overtake();
+	car.overtake();
+	car.overtake();
+	JOptionPane.showMessageDialog(null, "For the seventh annual racing cup, i'm proud to say that "+name+" is the winner! Congrats!");
 	}
 }
---
+++ src/_02_cat/KatRun.java
@@ -0,0 +1,8 @@
+package _02_cat;
+import javax.swing.JOptionPane;
+
+public class KatRun {
+Cat cat = new Cat("not-a-cat");
+cat.meow();
+cat.printName
+}
